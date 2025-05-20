8aaa5456a87520575693ef8723a845326df39f98
enjaku4 <enjaku4@gmail.com>
2025-05-21 00:59:18 +0200

    fix controllers

--- spec/support/controllers.rb
+++ spec/support/controllers.rb
@@ -1,6 +1,10 @@
 require "action_controller"
 require "action_view"
 
+class ApplicationController < ActionController::Base
+  # TODO
+end
+
 class DummyController < ApplicationController
   # TODO
 end
