f5138800c612011167d379e2da54443edd923729
enjaku4 <enjaku4@gmail.com>
2025-05-23 15:55:47 +0200

    update schema

--- spec/support/schema.rb
+++ spec/support/schema.rb
@@ -1,8 +1,21 @@
 ActiveRecord::Schema.define do
   self.verbose = false
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
+    t.text "hashed_password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "veri_sessions", force: :cascade do |t|
+    t.string "hashed_token", null: false
+    t.datetime "expires_at", null: false
+    t.integer "authenticatable_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["hashed_token"], name: "index_veri_sessions_on_hashed_token", unique: true
+    t.index ["authenticatable_id"], name: "index_veri_sessions_on_authenticatable_id"
+  end
+
+  add_foreign_key "veri_sessions", "users", column: "authenticatable_id"
 end
