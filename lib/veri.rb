fd99251e6b4c47429a918a86104e017a217e722f
enjaku4 <enjaku4@gmail.com>
2025-05-23 11:08:11 +0200

    require all files

--- lib/veri.rb
+++ lib/veri.rb
@@ -1,8 +1,23 @@
+require_relative "veri/version"
+
+require "active_record"
+require "active_support"
+
+require_relative "veri/password/argon2"
+require_relative "veri/password/bcrypt"
+require_relative "veri/password/scrypt"
+
+require_relative "veri/configuration"
+
+require_relative "veri/controllers/concerns/authentication"
+require_relative "veri/models/concerns/authenticatable"
+
+require_relative "veri/railtie"
+
 module Veri
   class Error < StandardError; end
   class ConfigurationError < Veri::Error; end
   class InvalidArgumentError < Veri::Error; end
-  class NotFoundError < Veri::Error; end
 
   def configure
     yield(Veri::Configuration.instance)
