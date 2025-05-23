1b65a1697b1730497e78e844d77a618ca8af232f
enjaku4 <enjaku4@gmail.com>
2025-05-23 11:05:22 +0200

    add password methods

--- lib/veri/models/concerns/authenticatable.rb
+++ lib/veri/models/concerns/authenticatable.rb
@@ -1,3 +1,7 @@
+require "argon2"
+require "bcrypt"
+require "scrypt"
+
 module Veri
   module Authenticatable
     extend ActiveSupport::Concern
@@ -8,6 +12,24 @@ module Veri
       @@included = name
     end
 
-    # TODO
+    def update_password(password)
+      validate_password(password)
+
+      update!(hashed_password: hasher.create(password))
+    end
+
+    def verify_password(password)
+      validate_password(password)
+
+      hasher.verify(password, hashed_password)
+    end
+
+    private
+
+    delegate :hasher, to: Veri::Configuration.instance
+
+    def validate_password(password)
+      raise Veri::InvalidArgumentError, "Password must be a string" unless password.is_a?(String)
+    end
   end
 end
